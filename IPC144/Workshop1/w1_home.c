5d093743193f8f3834ae90f788fba89929a6d62a
Devrun89 <73850545+Devrun89@users.noreply.github.com>
2020-12-08 15:34:29 -0500

    IPC_WORKSHOP1
    IPC_WORKSHOP1

---
+++ IPC144/Workshop1/w1_home.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("********************************\n");
+	printf("*** Welcome to C programming ***\n");
+	printf("********************************\n");
+
+	return 0;
+}
---
+++ IPC144/Workshop1/w1_lab.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf(" *** Welcome to C programming *** ");
+
+	return 0;
+}
